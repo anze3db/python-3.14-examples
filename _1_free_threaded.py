3f435b60751bc5317865f50c6337e12727f9e4f5
Anže Pečar <anze@pecar.me>
2025-07-23 10:44:14 +0100

    Add free threaded python example

---
+++ _1_free_threaded.py
@@ -0,0 +1,33 @@
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+
+def fib_task(n: int) -> int:
+    if n <= 1:
+        return n
+    return fib_task(n - 1) + fib_task(n - 2)
+
+
+def sleep_task(delay: float = 0.1):
+    import time
+
+    time.sleep(delay)
+
+
+tasks = os.cpu_count() or 2
+
+
+def run_fib():
+    with ThreadPoolExecutor(max_workers=tasks) as executor:
+        for _ in range(tasks):
+            executor.submit(fib_task, 35)
+
+
+def run_sleep():
+    with ThreadPoolExecutor(max_workers=tasks) as executor:
+        for _ in range(tasks):
+            executor.submit(sleep_task, 0.1)
+
+
+# uv run --python 3.14t python -m timeit -n 1 -r 1 -s 'import free_threaded' 'free_threaded.run_fib()'
+# uv run --python 3.14t python -m timeit -n 1 -r 1 -s 'import free_threaded' 'free_threaded.run_sleep()'
