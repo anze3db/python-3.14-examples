3590ddbfba59aa0bf86a67c79159ab4f848d9654
Anže Pečar <anze@pecar.me>
2025-07-23 23:18:29 +0100

    Remove Python 2 example (no time)

--- _07_exception_parantheses.py
+++ _07_exception_parantheses.py
@@ -10,11 +10,5 @@ try:
 except TimeoutError, ValueError:
     print("Something went wrong! Or did it?")
 
-# Python 2:
-try:
-    raise ValueError()
-except ValueError, e:
-    print("Caught a ValueError:", e)
 
 # python3.14 _07_exception_parantheses.py
-# docker run -it --rm -v $(pwd):/data  python:3.14-rc-bookworm python
