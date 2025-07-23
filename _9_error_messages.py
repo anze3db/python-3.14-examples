065d10c3f504796f727187d08b463164a48554bd
Anže Pečar <anze@pecar.me>
2025-07-23 11:07:12 +0100

    Error messags example

---
+++ _9_error_messages.py
@@ -0,0 +1,26 @@
+# whille True:
+#     pass
+
+# asynch def fetch_data():
+#     pass
+
+# raisee ValueError("Error")
+
+# x, y, z = 1, 2, 3, 4
+
+# if True:
+#     pass
+# else:
+#     pass
+# elif "2123":
+#     pass
+
+# x = 1 if True else pass
+
+# "The interesting object "The important object" is very important"
+
+# ub"Hellow"
+
+# import ast as []
+
+# s = {{"unhasable": "dict"}}
