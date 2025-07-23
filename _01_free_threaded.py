bd45c5bd650d6247215b5afa52700f879f08c77b
Anže Pečar <anze@pecar.me>
2025-07-23 17:30:57 +0100

    No time to sleep

--- _01_free_threaded.py
+++ _01_free_threaded.py
@@ -8,12 +8,6 @@ def fib_task(n: int) -> int:
     return fib_task(n - 1) + fib_task(n - 2)
 
 
-def sleep_task(delay: float = 0.1):
-    import time
-
-    time.sleep(delay)
-
-
 tasks = os.cpu_count() or 2
 
 
@@ -23,11 +17,4 @@ def run_fib():
             executor.submit(fib_task, 35)
 
 
-def run_sleep():
-    with ThreadPoolExecutor(max_workers=tasks) as executor:
-        for _ in range(tasks):
-            executor.submit(sleep_task, 0.1)
-
-
 # python3.14 -m timeit -n 1 -r 1 -s 'import _01_free_threaded' '_01_free_threaded.run_fib()'
-# python3.14 -m timeit -n 1 -r 1 -s 'import _01_free_threaded' '_01_free_threaded.run_sleep()'
